ca1eb0ac58f7bf21caa52f81b9e5c36fc915b794
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-03-12 19:40:16 +0300

    feat: `nav`

--- src/components/sections/header/Nav.jsx
+++ src/components/sections/header/Nav.jsx
@@ -2,9 +2,23 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <nav className='nav'>
-      
-    </nav>
+    <ul className='nav'>
+      <li className='nav--item'>
+        Услуги
+      </li>
+      <li className='nav--item'>
+        Виджеты
+      </li>
+      <li className='nav--item'>
+        Интеграции
+      </li>
+      <li className='nav--item'>
+        Кейсы
+      </li>
+      <li className='nav--item'>
+        Сертификаты
+      </li>
+    </ul>
   );
 };
 
