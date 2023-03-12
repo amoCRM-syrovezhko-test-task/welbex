4525a9374be5b0b2e172d5e97f517cd5df61aff1
Nikolai Syrovezhko <syrovezhko@list.ru>
2023-03-12 20:35:53 +0300

    feat: left `main` block

--- src/components/sections/Main.jsx
+++ src/components/sections/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import MainFirst from './main/MainFirst';
 
 const Main = () => {
   return (
     <main className='main'>
-      
+      <MainFirst />
     </main>
   );
 };
